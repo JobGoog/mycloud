91cd5ccd696ebf64d11d3851ae19097021f0cd4a
JobGoog <roro425052@gmail.com>
2025-07-22 03:56:05 +0300

    m2

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://31.31.207.254:8000'; // на сервере
+const API_BASE_URL = 'http://194.58.114.177:8000'; // на сервере
 // const API_BASE_URL = 'http://localhost:8000'; // на локалке
 
 export default API_BASE_URL;
