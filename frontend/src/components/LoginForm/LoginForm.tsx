30fbdacb9b3f6460c58a18a70be8fa568e6e8441
JobGoog <roro425052@gmail.com>
2025-08-04 21:10:13 +0300

    Fix all bags

--- frontend/src/components/LoginForm/LoginForm.tsx
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -5,42 +5,17 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export const LoginForm: React.FC<{ updateRole: (role: string, is_superuser: boolean) => void }> = ({ updateRole }) => {
-    const [username, setUsername] = useState<string>('');  // Состояние для хранения имени пользователя, вводимого в форму
-    const [password, setPassword] = useState<string>('');  // Состояние для хранения пароля, вводимого в форму
-    const [error, setError] = useState<string>('');  // Состояние для хранения сообщения об ошибке в случае неудачной аутентификации
-    const navigate = useNavigate();  // Хук для навигации между страницами приложения в зависимости от роли пользователя
+    const [username, setUsername] = useState<string>('');  
+    const [password, setPassword] = useState<string>('');  
+    const [error, setError] = useState<string>('');  
+    const navigate = useNavigate();  
 
-    /**
-     * handleSubmit - Обработчик отправки формы для входа пользователя.
-     * 
-     * Эта асинхронная функция выполняется при отправке формы входа. Она предотвращает 
-     * стандартное поведение формы, отправляет POST-запрос на сервер для аутентификации 
-     * пользователя и обрабатывает ответ. 
-     * 
-     * Основные шаги:
-     * 1. Предотвращает стандартное поведение формы с помощью e.preventDefault().
-     * 2. Отправляет POST-запрос на '/api/login' с данными пользователя (имя пользователя и пароль).
-     * 3. Если ответ от сервера не успешен (response.ok === false), выбрасывает ошибку 
-     *    с сообщением 'Некорректный логин или пароль'.
-     * 4. Если аутентификация успешна, извлекает токен и роль пользователя из ответа сервера:
-     *    - Если роль 'admin' или указан флаг 'is_superuser', перенаправляет на 
-     *      административную панель ('/admin').
-     *    - Если роль 'user', перенаправляет на панель файлового хранилища ('/storage/{userId}').
-     * 5. Обрабатывает любые ошибки, возникающие во время запроса:
-     *    - Устанавливает сообщение об ошибке с помощью setError().
-     * 
-     * Ожидаемые данные от сервера: 
-     * - auth_token: Токен аутентификации (строка)
-     * - role: Роль пользователя (строка, возможные значения: 'admin', 'user')
-     * - id_user: ID пользователя (число)
-     * - is_superuser: Флаг, указывающий, является ли пользователь суперпользователем (boolean)
-     */
     
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         try {
-            const response = await fetch(`${API_BASE_URL}/auth/token/login/`, {
+            const response = await fetch(`${API_BASE_URL}/api/v1/auth/token/login/`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -76,10 +51,10 @@ export const LoginForm: React.FC<{ updateRole: (role: string, is_superuser: bool
             const { role, id_user, is_superuser } = data_user;
             localStorage.setItem('role', role);
             
-            // Обновляем роль
+            
             updateRole(role, is_superuser);
             
-            // Переход на соответствующую страницу в зависимости от роли
+            
             if (role === 'admin' || is_superuser) {
                 navigate('/admin');
             } else {
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://193.227.241.76:8000'; // на сервере
-// const API_BASE_URL = 'http://localhost:8000'; // на локалке
+const API_BASE_URL = 'http://79.174.83.69';
+
 
 export default API_BASE_URL;
 
